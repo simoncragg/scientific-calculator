a985d09714974c06935b1208e08a77a7eb9aedaa
Simon Cragg <22366916+simoncragg@users.noreply.github.com>
2024-03-16 18:24:19 +0000

    refactor: use uniform angle conversion equations

--- src/utils/convertFromRadians.ts
+++ src/utils/convertFromRadians.ts
@@ -3,7 +3,7 @@ import type { AngleUnit } from "../types";
 export function convertFromRadians(angle: number, unit: AngleUnit) {
   switch (unit) {
     case "deg": return angle * (180 / Math.PI);
-    case "grad": return angle / (Math.PI / 200);
+    case "grad": return angle * (200 / Math.PI);
     default: return angle;
   }
 }
--- src/utils/convertToRadians.ts
+++ src/utils/convertToRadians.ts
@@ -3,7 +3,7 @@ import { AngleUnit } from "../types";
 export function convertToRadians(angle: number, unit: AngleUnit) {
     switch (unit) {
       case "deg": return angle * (Math.PI / 180);
-      case "grad": return angle / (200 / Math.PI);
+      case "grad": return angle * (Math.PI / 200);
       default: return angle;
     }
-  }
+}
